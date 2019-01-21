b874d0f1554c4d6739f87a69922a5f0abe89ffe3
Gagandeep07-Kaur <gagandeep07kaur@gmail.com>
2019-01-21 11:54:17 +0530

    choose a file

--- task.js
+++ task.js
@@ -1,9 +1,9 @@
- // = ["Chandigarh", "Patna", "Dispur", "Chandigarh", "Raipur"];
-// var stat =  ["Punjab", "Bihar", "Assam", "Haryana", "Chhattisgarh"];
-//
-//
-//
-// document.getElementById("capital").innerHTML = (Math.floor(Math.random() * cap.length));
+ cap = ["Chandigarh", "Patna", "Dispur", "Chandigarh", "Raipur"];
+var stat =  ["Punjab", "Bihar", "Assam", "Haryana", "Chhattisgarh"];
+
+
+
+document.getElementById("capital").innerHTML = (Math.floor(Math.random() * cap.length));
 
 function validate () {
   if(document.myForm.fname.value == "") {
